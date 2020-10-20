38aecff40517a537cfedcd9e80bec1d0295fab7f
rohitchauhan3 <67888740+rohitchauhan3@users.noreply.github.com>
2020-10-20 21:15:53 +0530

    lighting_update_map
    all all light map change the light map script

--- MainProject/Assets/Scripts/LightingTest.cs
+++ MainProject/Assets/Scripts/LightingTest.cs
@@ -10,16 +10,26 @@ public class LightingTest : MonoBehaviour
     public Texture2D aa2;
     public Texture2D a3;
     public Texture2D aa3;
+    public Texture2D a4;
+    public Texture2D aa4;
+    public Texture2D a5;
+    public Texture2D aa5;
     public Material garge;
     public Material bar;
     public Material ship;
+    public Material train;
+    public Material country_side;
     public Camera cam1;
     public Camera cam2;
     public Camera cam3;
+    public Camera cam4;
+    public Camera cam5;
 
     private LightmapData[] lightmapData1 = new LightmapData[1];
     private LightmapData[] lightmapData2 = new LightmapData[1];
     private LightmapData[] lightmapData3 = new LightmapData[1];
+    private LightmapData[] lightmapData4 = new LightmapData[1];
+    private LightmapData[] lightmapData5 = new LightmapData[1];
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +45,14 @@ public class LightingTest : MonoBehaviour
         lightmapData3[0].lightmapDir = a3;
         lightmapData3[0].lightmapColor = aa3;
 
+        lightmapData4[0] = new LightmapData();
+        lightmapData4[0].lightmapDir = a4;
+        lightmapData4[0].lightmapColor = aa4;
+
+        lightmapData5[0] = new LightmapData();
+        lightmapData5[0].lightmapDir = a5;
+        lightmapData5[0].lightmapColor = aa5;
+
     }
 
     // Update is called once per frame
@@ -47,6 +65,8 @@ public class LightingTest : MonoBehaviour
             cam1.enabled = true;
             cam2.enabled = false;
             cam3.enabled = false;
+            cam4.enabled = false;
+            cam5.enabled = false;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
@@ -55,6 +75,8 @@ public class LightingTest : MonoBehaviour
             cam1.enabled = false;
             cam2.enabled = true;
             cam3.enabled = false;
+            cam4.enabled = false;
+            cam5.enabled = false;
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
@@ -63,6 +85,28 @@ public class LightingTest : MonoBehaviour
             cam1.enabled = false;
             cam2.enabled = false;
             cam3.enabled = true;
+            cam4.enabled = false;
+            cam5.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LightmapSettings.lightmaps = lightmapData3;
+            RenderSettings.skybox = train;
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = false;
+            cam4.enabled = true;
+            cam5.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LightmapSettings.lightmaps = lightmapData3;
+            RenderSettings.skybox = country_side;
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = false;
+            cam4.enabled = false;
+            cam5.enabled = true;
         }
 
     }
