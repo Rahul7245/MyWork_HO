0f7e35cb38a5464ea96b49d3f8cf5864cf4f152e
atulafternow <atul@afternow.io>
2020-10-20 15:34:27 +0530

    Added Loading logic

---
+++ MainProject/Assets/NewScripts/AtulScripts/IntroCanvasHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System;
+
+public class IntroCanvasHandler : MonoBehaviour 
+{
+    #region Private Variables 
+
+    [SerializeField]
+	private Slider m_progressBar;
+	[SerializeField]
+	private float m_barFillDuration = 4;
+
+	#endregion
+
+	void Start () 
+	{
+		m_progressBar.value = 0;
+		m_progressBar.DOValue(1, m_barFillDuration);
+	}
+}
