fe9db45e038d253efe8bac21cd3ffb6d20d0fc5d
atulafternow <atul@afternow.io>
2020-10-27 03:31:36 +0530

    Update ShootSceneScript.cs

--- MainProject/Assets/models/Environments/garage/ShootSceneScript.cs
+++ MainProject/Assets/models/Environments/garage/ShootSceneScript.cs
@@ -19,7 +19,7 @@ public class ShootSceneScript : MonoBehaviour
     public LightingManager lightingTest;
     CameraShaker ch;
     Controller con;
-    
+
     private void Awake()
     {
         pointGroup = new GroupOfPoints[5];
@@ -35,43 +35,45 @@ public class ShootSceneScript : MonoBehaviour
         pointGroup[4] = GaragePoints.Instance.getShipEnvironmentPoints();
     }
     int EnvironmentNum;
-   void setEnvironment(int en) {
-        lightingTest.ChangeLightingData(en);
-        EnvironmentNum = en;
+    void setEnvironment(EnviromentType enviromentType)
+    {
+        lightingTest.ChangeLightingData(enviromentType);
     }
-    public void InitializeScene(int en) {
+    public void InitializeScene(int en)
+    {
+        EnvironmentNum = en;
         Debug.Log("EN " + en);
-        setEnvironment(en);
+        setEnvironment((EnviromentType)en);
         setShootPoint(en);
         setBurglarStartPoint();
         setBurglarEndPoint();
     }
-    
-    void setShootPoint(int en) {
-        EnvironmentNum = en;
-        print(pointGroup[EnvironmentNum].shootPositions[0].transform.position);
-        character.transform.position = pointGroup[EnvironmentNum].shootPositions[0].transform.position;
-        ch.gameObject.transform.localRotation = pointGroup[EnvironmentNum].shootPositions[0].transform.localRotation;
-        con.setMRotations();
-        // character.transform.rotation = pointGroup[EnvironmentNum].shootPositions[0].transform.rotation;
 
+    void setShootPoint(int en)
+    {
+        print(pointGroup[en].shootPositions[0].transform.position);
+        character.transform.position = pointGroup[en].shootPositions[0].transform.position;
+        ch.gameObject.transform.localRotation = pointGroup[en].shootPositions[0].transform.localRotation;
+        con.setMRotations();
     }
-   public  void setBurglerNoneAnimation() {
+    public void setBurglerNoneAnimation()
+    {
         for (int i = 0; i < 5; i++)
         {
 
             m_burglar[i].NoneAnimation();
-            
+
         }
     }
     List<int> listEndPoints = new List<int>();
-    void setBurglarStartPoint() {
-        List<int> list = new List<int>{ 19, 12, 0, 18, 2, 11, 17, 5, 7, 16, 10, 6, 15, 3, 9, 1, 14, 8, 4, 13 };
+    void setBurglarStartPoint()
+    {
+        List<int> list = new List<int> { 19, 12, 0, 18, 2, 11, 17, 5, 7, 16, 10, 6, 15, 3, 9, 1, 14, 8, 4, 13 };
         int rint;
         for (int i = 0; i < 5; i++)
         {
-             m_burglar[i].GetComponent<NavMeshAgent>().enabled = false;
-             rint = Random.Range(0, list.Count - 1);
+            m_burglar[i].GetComponent<NavMeshAgent>().enabled = false;
+            rint = Random.Range(0, list.Count - 1);
             m_burglar[i].transform.position = pointGroup[EnvironmentNum].groupOfPoints[list.ElementAt(rint)].startPoint.transform.position;
             m_burglar[i].SetStartPosition(pointGroup[EnvironmentNum].groupOfPoints[list.ElementAt(rint)].startPoint.transform);
             listEndPoints.Add(list.ElementAt(rint));
@@ -83,41 +85,47 @@ public class ShootSceneScript : MonoBehaviour
     {
         for (int i = 0; i < 5; i++)
         {
-            if(pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints.Count>0)
-            m_burglar[i].SetDestination(pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints[Random.Range(0, pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints.Count-1)].transform);
+            if (pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints.Count > 0)
+                m_burglar[i].SetDestination(pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints[Random.Range(0, pointGroup[EnvironmentNum].groupOfPoints[listEndPoints[i]].endPoints.Count - 1)].transform);
         }
         listEndPoints.Clear();
     }
 
-   public void AddShotEffects() {
+    public void AddShotEffects()
+    {
         Time.timeScale = 0.6f;
-       // VigneteEffect.Instance.VigneteEffectStart();
+        // VigneteEffect.Instance.VigneteEffectStart();
     }
 
-    public void CameraEffect(string msg) {
+    public void CameraEffect(string msg)
+    {
         con.InactiveScope();
         StartCoroutine(AfterDieEffect(msg));
     }
-    IEnumerator AfterDieEffect(string msg) {
+    IEnumerator AfterDieEffect(string msg)
+    {
         yield return new WaitForSeconds(2f);
         DisplayPoints(msg);
         Time.timeScale = 1f;
     }
-    public void DisplayPoints(string msg) {
+    public void DisplayPoints(string msg)
+    {
         PointsCanvas.SetActive(true);
-       
+
         PointsCanvas.GetComponentInChildren<TextMeshProUGUI>().text = msg;
 
     }
-    public void LoadScene() {
+    public void LoadScene()
+    {
         StartCoroutine(LoadBirdViewScene());
     }
-     IEnumerator LoadBirdViewScene() {
+    IEnumerator LoadBirdViewScene()
+    {
         yield return new WaitForSeconds(5);
         PointsCanvas.SetActive(false);
         gameManager.GetComponent<SwitchCamera>().ShootCameraEnable(false);
-        lightingTest.ChangeLightingData(4); 
-        
+        lightingTest.ChangeLightingData(EnviromentType.BirdView);
+
         ShootSceneStateManager.Instance.ToggleAppState(ShootState.Result);
 
     }
