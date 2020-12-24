627e5e2ce12c934895bb1be74a2b03a0da601210
rohitchauhan3 <67888740+rohitchauhan3@users.noreply.github.com>
2020-12-24 16:08:29 +0530

    added the opetion  to cntroll the looping of audio clip

--- MainProject/Assets/_Main Project/Scripts/AtulScripts/Managers/AudioManager.cs
+++ MainProject/Assets/_Main Project/Scripts/AtulScripts/Managers/AudioManager.cs
@@ -89,9 +89,10 @@ public class AudioManager : MonoBehaviour
     public AudioClip AC_Die;
     public AudioClip AC_Shoot;
 
-    public void PlayAudio(AudioSourceType audioSource, AudioCLips audioCLips)
+    public void PlayAudio(AudioSourceType audioSource, AudioCLips audioCLips, bool loopClip = false)
     {
-        SetCip(audioSource, audioCLips);
+        
+        SetCip(audioSource, audioCLips, loopClip);
         PlayAudioSource(audioSource);
     }
 
@@ -134,7 +135,7 @@ public class AudioManager : MonoBehaviour
                 break;
         }
     }
-    private void SetCip(AudioSourceType audioSource , AudioCLips audioCLips)
+    private void SetCip(AudioSourceType audioSource , AudioCLips audioCLips, bool loopClip)
     {
         switch (audioSource)
         {
@@ -142,67 +143,67 @@ public class AudioManager : MonoBehaviour
                 switch (audioCLips)
                 {
                     case AudioCLips.AC_Character01_selection:
-                        SetAudioClip(AC_Character01_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character01_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Character02_selection:
-                        SetAudioClip(AC_Character02_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character02_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Character03_selection:
-                        SetAudioClip(AC_Character03_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character03_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Character04_selection:
-                        SetAudioClip(AC_Character04_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character04_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Character05_selection:
-                        SetAudioClip(AC_Character05_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character05_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Character06_selection:
-                        SetAudioClip(AC_Character06_selection, AS_AnimEf);
+                        SetAudioClip(AC_Character06_selection, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Self_die:
-                        SetAudioClip(AC_Hurdule_Self_die, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Self_die, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Self_add2:
-                        SetAudioClip(AC_Hurdule_Self_add2, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Self_add2, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Self_sub2:
-                        SetAudioClip(AC_Hurdule_Self_sub2, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Self_sub2, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Self_Skip:
-                        SetAudioClip(AC_Hurdule_Self_Skip, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Self_Skip, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Self_life:
-                        SetAudioClip(AC_Hurdule_Self_life, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Self_life, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Rival_die:
-                        SetAudioClip(AC_Hurdule_Rival_die, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Rival_die, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Rival_skip:
-                        SetAudioClip(AC_Hurdule_Rival_skip, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Rival_skip, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Rival_Help:
-                        SetAudioClip(AC_Hurdule_Rival_Help, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Rival_Help, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Hurdule_Rival_Back:
-                        SetAudioClip(AC_Hurdule_Rival_Back, AS_AnimEf);
+                        SetAudioClip(AC_Hurdule_Rival_Back, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Win:
-                        SetAudioClip(AC_Win, AS_AnimEf);
+                        SetAudioClip(AC_Win, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Running_track:
-                        SetAudioClip(AC_Running_track, AS_AnimEf);
+                        SetAudioClip(AC_Running_track, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Timmer:
-                        SetAudioClip(AC_Timmer, AS_AnimEf);
+                        SetAudioClip(AC_Timmer, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_SlowMotion:
-                        SetAudioClip(AC_SlowMotion, AS_AnimEf);
+                        SetAudioClip(AC_SlowMotion, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Die:
-                        SetAudioClip(AC_Die, AS_AnimEf);
+                        SetAudioClip(AC_Die, AS_AnimEf, loopClip);
                         break;
                     case AudioCLips.AC_Shoot:
-                        SetAudioClip(AC_Shoot, AS_AnimEf);
+                        SetAudioClip(AC_Shoot, AS_AnimEf, loopClip);
                         break;                
                 }
                 break;
@@ -210,7 +211,7 @@ public class AudioManager : MonoBehaviour
                 switch (audioCLips)
                 {
                     case AudioCLips.AC_BgMusic:
-                        SetAudioClip(AC_BG, AS_BackGround);
+                        SetAudioClip(AC_BG, AS_BackGround, loopClip);
                         break;
                 }
                 break;
@@ -218,22 +219,22 @@ public class AudioManager : MonoBehaviour
                 switch (audioCLips)
                 {
                     case AudioCLips.AC_BarView:
-                        SetAudioClip(AC_BarView, AS_Env);
+                        SetAudioClip(AC_BarView, AS_Env, loopClip);
                         break;
                     case AudioCLips.AC_BirdView:
-                        SetAudioClip(AC_BirdView, AS_Env);
+                        SetAudioClip(AC_BirdView, AS_Env, loopClip);
                         break;
                     case AudioCLips.AC_GeragView:
-                        SetAudioClip(AC_GeragView, AS_Env);
+                        SetAudioClip(AC_GeragView, AS_Env, loopClip);
                         break;
                     case AudioCLips.AC_ShipView:
-                        SetAudioClip(AC_ShipView, AS_Env);
+                        SetAudioClip(AC_ShipView, AS_Env, loopClip);
                         break;
                     case AudioCLips.AC_TrainView:
-                        SetAudioClip(AC_TrainView, AS_Env);
+                        SetAudioClip(AC_TrainView, AS_Env, loopClip);
                         break;
                     case AudioCLips.AC_Pages:
-                        SetAudioClip(AC_Pages, AS_Env);
+                        SetAudioClip(AC_Pages, AS_Env, loopClip);
                         break;
                     
                 }
@@ -241,9 +242,9 @@ public class AudioManager : MonoBehaviour
 
         }
     }
-    private void SetAudioClip(AudioClip audioClip , AudioSource audioSource)
+    private void SetAudioClip(AudioClip audioClip , AudioSource audioSource, bool loopClip)
     {
-        Debug.Log("!#$ SetAudioClip !#@");
+        audioSource.loop = loopClip;
         if (audioSource.isPlaying)
             audioSource.Stop();
         if(audioClip != null)
