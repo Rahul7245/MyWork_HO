f961d134c28ec38e328e2f582c0364e3b117afd6
Atul Singh <atulsingh26@live.com>
2021-01-10 02:35:15 +0530

    ficing Running Track side ui icon manage statrt

---
+++ MainProject/Assets/_Main Project/Scripts/AtulScripts/Utility/CharactersProperty.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CharactersProperty : MonoBehaviour
+{
+    public Image CharImage;
+    public TextMeshProUGUI CharName;
+    public TextMeshProUGUI CharPoints;
+
+    public void SetDeafult(Sprite charSprite, string charName, string charPoints)
+    {
+        CharImage.sprite = charSprite;
+        CharName.text = charName;
+        CharPoints.text = charPoints;
+    }
+}
