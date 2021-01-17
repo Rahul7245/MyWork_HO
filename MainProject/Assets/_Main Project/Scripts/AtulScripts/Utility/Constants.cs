58b23abff0944581b882cdd984440e6da7777aa4
rohitchauhan3 <67888740+rohitchauhan3@users.noreply.github.com>
2021-01-18 00:45:34 +0530

    about character text change

--- MainProject/Assets/_Main Project/Scripts/AtulScripts/Utility/Constants.cs
+++ MainProject/Assets/_Main Project/Scripts/AtulScripts/Utility/Constants.cs
@@ -19,10 +19,10 @@ public static class Constants
 
     public const string Msg_CharacterSelected = "Character Selected";
     public const string Msg_WantToExitGame = "You really wanna exit the game \nthis means you will lose the game";
-    public const string Msg_Char00_Dis = "About CHARACTER_01\nMale\nHATS ON THE FANTASHY GAME";
-    public const string Msg_Char01_Dis = "About CHARACTER_02\nMale\nHATS ON THE FANTASHY GAME";
-    public const string Msg_Char02_Dis = "About CHARACTER_03\nMale\nHATS ON THE FANTASHY GAME";
-    public const string Msg_Char03_Dis = "About CHARACTER_04\nMale\nHATS ON THE FANTASHY GAME";
-    public const string Msg_Char04_Dis = "About CHARACTER_05\nMale\nHATS ON THE FANTASHY GAME";
-    public const string Msg_Char05_Dis = "About CHARACTER_06\nMale\nHATS ON THE FANTASHY GAME";
+    public const string Msg_Char00_Dis = "      Dasch     \nThe Nobleman\nHeight 5''11''\nAthlete body type \nAge 28 \nStrength - sharp shooter\n\nHats On Fantashy Game";
+    public const string Msg_Char01_Dis = "      Bella     \nLove of Paradise\nHeight 5''6''\nStunning \nAge 19 \nStrength - sharp shooter\n\nHats On Fantashy Game";
+    public const string Msg_Char02_Dis = "     Morakko    \nLion of Hakuna\nHeight 5''8''\nBroad \nAge 24 \nStrength - sharp shooter\n\nHats On Fantashy Game";
+    public const string Msg_Char03_Dis = "      Kastor    \nThe Boozer\nHeight 5''7''\nAthlete body type \nAge 21 \nStrength - sharp shooter\n\nHats On Fantashy Game";
+    public const string Msg_Char04_Dis = "      Ricado    \nThe Hunk\nHeight 5''10''\nRipped \nAge 20 \nStrength - sharp shooter\n\nHats On Fantashy Game";
+    public const string Msg_Char05_Dis = "       Jaxi     \nThe Sparkplug\nHeight 5''8''\nAthlete body type \nAge 19 \nStrength - sharp shooter\n\nHats On Fantashy Game";
 }
